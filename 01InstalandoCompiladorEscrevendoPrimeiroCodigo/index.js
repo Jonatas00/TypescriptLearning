b0e4868ad6e502387bfc79d0be203895a14ec9ab
Jonatas <djonatas31@gmail.com>
2023-08-18 09:45:25 -0300

    Delete

--- 01InstalandoCompiladorEscrevendoPrimeiroCodigo/index.js
+++
@@ -1,9 +0,0 @@
-var inputA = document.getElementById("inputA");
-var inputB = document.getElementById("inputB");
-var button = document.getElementById("button");
-function sum(a, b) {
-    return a + b;
-}
-button.addEventListener("click", function () {
-    console.log(sum(Number(inputA.value), Number(inputB.value)));
-});
